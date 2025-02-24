57ae7c46003718a7caf0362040c8b07f41c8fad8
Garmoska <leonov.eugene@gmail.com>
2025-02-24 11:43:11 +0100

    Update app.js

--- week02/simple-shop/app.js
+++ week02/simple-shop/app.js
@@ -11,7 +11,6 @@ const server = http.createServer(async (req, res) => {
         res.write("<html>");
         res.write("<body><b>I am OK!</b></body>");
         res.write("</html>");
-        return res.end();
     } else if (req.url.startsWith("/employees") && req.method === "GET") {
         const employees = await dml.readEmployees();
 
@@ -34,8 +33,50 @@ const server = http.createServer(async (req, res) => {
         res.write("</ul>");
         res.write("</body>");
         res.write("</html>");
-        return res.end();
+    }  else if (req.url.startsWith("/goods") && req.method === "GET") {
+        const categories = await dml.readCategories();
+        const goods = await dml.readGoods();
+
+        const parsedURL = url.parse(req.url, true);
+        let ddCategories = parsedURL.query.ddCategories;
+        if (ddCategories == null) ddCategories = "0";
+        console.log("ddCategories =" + ddCategories);
+
+        const goodsPerCategory = [];
+        for (const g of goods) {
+            if (g.category.toString() === ddCategories) {
+                goodsPerCategory.push(g.name);
+            }
+        }
+
+        res.write("<html>");
+        res.write("<body>");
+        res.write("<form>");
+        res.write("<div>");
+        res.write("<select name='ddCategories'>");
+        for (const category of categories) {
+            res.write("<option value='" + category.value + "' " + (category.value.toString() === ddCategories ? "selected" : "") + " >" + category.name + "</option>");
+        }
+        res.write("</select>");
+        res.write("</div>");
+
+        res.write("<div>");
+        res.write("<ul>");
+        for (const g of goodsPerCategory) {
+            res.write("<li>");
+            res.write(g);
+            res.write("</li>");
+        }
+        res.write("</ul>");
+        res.write("</div>");
+
+        res.write("<div><button>Submit</button></div>");
+
+        res.write("</form>");
+        res.write("</body>");
+        res.write("</html>");
     }
+    return res.end();
 });
 
 server.listen(3002);
